747cb2febf1c5c112f18f9520af6ee6ed30f3186
MaikeBezerra <maike_ti@hotmail.com>
2019-11-22 17:49:59 -0300

    Removendo god class TurmaController

---
+++ prontuario-v1.3.1/aplicacao/prontuario/src/main/java/ufc/npi/prontuario/controller/ProfessorTurmaController.java
@@ -0,0 +1,69 @@
+package ufc.npi.prontuario.controller;
+
+import static ufc.npi.prontuario.util.ConfigurationConstants.PERMISSAO_ADMINISTRACAO;
+import static ufc.npi.prontuario.util.ExceptionSuccessConstants.ERROR;
+import static ufc.npi.prontuario.util.ExceptionSuccessConstants.SUCCESS;
+import static ufc.npi.prontuario.util.ExceptionSuccessConstants.SUCCESS_DESVINCULAR_PROFESSOR;
+import static ufc.npi.prontuario.util.ExceptionSuccessConstants.SUCCESS_VINCULAR_PROFESSOR;
+import static ufc.npi.prontuario.util.FragmentsConstants.FRAGMENT_PROFESSORES;
+import static ufc.npi.prontuario.util.RedirectConstants.REDIRECT_DETALHES_TURMA;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.servlet.ModelAndView;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
+
+import ufc.npi.prontuario.exception.ProntuarioException;
+import ufc.npi.prontuario.model.Servidor;
+import ufc.npi.prontuario.model.Turma;
+import ufc.npi.prontuario.service.TurmaService;
+
+@Controller
+@RequestMapping("/turma")
+public class ProfessorTurmaController {
+
+	@Autowired
+	private TurmaService turmaService;
+	
+	@PreAuthorize(PERMISSAO_ADMINISTRACAO)
+	@PostMapping("/{idTurma}/adicionar-professor")
+	public ModelAndView adicionarProfessor(@PathVariable("idTurma") Turma turma, 
+			@ModelAttribute("novosProfessores") Turma novosProfessores, RedirectAttributes attributes){
+		
+		if(novosProfessores.getProfessores() != null && !novosProfessores.getProfessores().isEmpty()){
+			turmaService.adicionarProfessorTurma(turma, novosProfessores.getProfessores());
+			attributes.addFlashAttribute(SUCCESS, SUCCESS_VINCULAR_PROFESSOR);
+		}
+		
+		return new ModelAndView(REDIRECT_DETALHES_TURMA + turma.getId());
+	}
+	
+	@GetMapping("/professores/")
+	public ModelAndView carregarProfessores(@RequestParam("idTurma") Turma turma ) {
+		ModelAndView modelAndView = new ModelAndView(FRAGMENT_PROFESSORES);
+		modelAndView.addObject("professores", turma != null ? turma.getProfessores() : null);
+		return modelAndView;
+	}
+	
+	@PreAuthorize(PERMISSAO_ADMINISTRACAO)
+	@GetMapping("/{idTurma}/remover-professor/{idProfessor}")
+	public ModelAndView removerProfessor(@PathVariable("idTurma") Turma turma, @PathVariable("idProfessor") Servidor professor,
+			RedirectAttributes attributes) {
+		
+		try {
+			turmaService.removerProfessor(turma, professor);
+			attributes.addFlashAttribute(SUCCESS, SUCCESS_DESVINCULAR_PROFESSOR);
+		} catch (ProntuarioException e) {
+			attributes.addFlashAttribute(ERROR, e.getMessage());
+		}
+		
+		return new ModelAndView(REDIRECT_DETALHES_TURMA + turma.getId());
+	}
+}
--- prontuario-v1.3.1/aplicacao/prontuario/src/main/java/ufc/npi/prontuario/controller/TurmaController.java
+++ prontuario-v1.3.1/aplicacao/prontuario/src/main/java/ufc/npi/prontuario/controller/TurmaController.java
@@ -7,13 +7,7 @@ import static ufc.npi.prontuario.util.ExceptionSuccessConstants.ERROR;
 import static ufc.npi.prontuario.util.ExceptionSuccessConstants.SUCCESS;
 import static ufc.npi.prontuario.util.ExceptionSuccessConstants.SUCCESS_ALTERAR_STATUS;
 import static ufc.npi.prontuario.util.ExceptionSuccessConstants.SUCCESS_CADASTRAR_TURMA;
-import static ufc.npi.prontuario.util.ExceptionSuccessConstants.SUCCESS_DESVINCULAR_PROFESSOR;
 import static ufc.npi.prontuario.util.ExceptionSuccessConstants.SUCCESS_EXCLUIR_TURMA;
-import static ufc.npi.prontuario.util.ExceptionSuccessConstants.SUCCESS_MATRICULAR_ALUNO;
-import static ufc.npi.prontuario.util.ExceptionSuccessConstants.SUCCESS_REMOVER_INSCRICAO;
-import static ufc.npi.prontuario.util.ExceptionSuccessConstants.SUCCESS_VINCULAR_PROFESSOR;
-import static ufc.npi.prontuario.util.FragmentsConstants.FRAGMENT_AJUDANTES;
-import static ufc.npi.prontuario.util.FragmentsConstants.FRAGMENT_PROFESSORES;
 import static ufc.npi.prontuario.util.FragmentsConstants.FRAGMENT_STATUS_TURMA;
 import static ufc.npi.prontuario.util.PagesConstants.FORMULARIO_ADICIONAR_TURMA;
 import static ufc.npi.prontuario.util.PagesConstants.PAGINA_DETALHES_TURMA;
@@ -36,16 +30,13 @@ import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.servlet.ModelAndView;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 import ufc.npi.prontuario.exception.ProntuarioException;
-import ufc.npi.prontuario.model.Aluno;
 import ufc.npi.prontuario.model.Servidor;
 import ufc.npi.prontuario.model.Turma;
 import ufc.npi.prontuario.model.Usuario;
-import ufc.npi.prontuario.service.AlunoService;
 import ufc.npi.prontuario.service.DisciplinaService;
 import ufc.npi.prontuario.service.ServidorService;
 import ufc.npi.prontuario.service.TurmaService;
@@ -63,9 +54,6 @@ public class TurmaController {
 	@Autowired
 	private ServidorService servidorService;
 
-	@Autowired
-	private AlunoService alunoService;
-	
 	@PreAuthorize(PERMISSOES_PROFESSOR_ADMINISTRACAO)
 	@GetMapping(value = "/listar")
 	public ModelAndView listarTurmas(Authentication auth) {
@@ -103,34 +91,6 @@ public class TurmaController {
 		return new ModelAndView(REDIRECT_DETALHES_TURMA + turma.getId());
 	}
 
-	@PreAuthorize(PERMISSAO_ADMINISTRACAO)
-	@PostMapping("/{idTurma}/inscrever")
-	public ModelAndView inscreverAluno(@PathVariable("idTurma") Turma turma,
-			@RequestParam("matricula") String matricula, RedirectAttributes attributes) {
-		
-		try {
-			turmaService.inscreverAluno(turma, matricula);
-			attributes.addFlashAttribute(SUCCESS, SUCCESS_MATRICULAR_ALUNO);
-		} catch (ProntuarioException e) {
-			attributes.addFlashAttribute(ERROR, e.getMessage());
-		}
-		
-		return new ModelAndView(REDIRECT_DETALHES_TURMA + turma.getId());
-	}
-
-	@PreAuthorize(PERMISSAO_ADMINISTRACAO)
-	@PostMapping("/{idTurma}/adicionar-professor")
-	public ModelAndView adicionarProfessor(@PathVariable("idTurma") Turma turma, 
-			@ModelAttribute("novosProfessores") Turma novosProfessores, RedirectAttributes attributes){
-		
-		if(novosProfessores.getProfessores() != null && !novosProfessores.getProfessores().isEmpty()){
-			turmaService.adicionarProfessorTurma(turma, novosProfessores.getProfessores());
-			attributes.addFlashAttribute(SUCCESS, SUCCESS_VINCULAR_PROFESSOR);
-		}
-		
-		return new ModelAndView(REDIRECT_DETALHES_TURMA + turma.getId());
-	}
-	
 	@PostAuthorize(PERMISSOES_ADMINISTRACAO_VERIFICACAO_PROFESSOR)
 	@GetMapping("/{idTurma}")
 	public ModelAndView visualizarDetalhes(@PathVariable("idTurma") Turma turma) {
@@ -158,21 +118,6 @@ public class TurmaController {
 		return modelAndView;
 	}
 	
-
-	@GetMapping("/ajudantes/")
-	public ModelAndView carregarAjudantes(@RequestParam("idTurma") Integer idTurma, @RequestParam("idResponsavel") Integer idResponsavel) {
-		ModelAndView modelAndView = new ModelAndView(FRAGMENT_AJUDANTES);
-		modelAndView.addObject("ajudantes", idTurma == null ? null : alunoService.buscarAjudantes(idTurma, alunoService.buscarPorId(idResponsavel)));
-		return modelAndView;
-	}
-
-	@GetMapping("/professores/")
-	public ModelAndView carregarProfessores(@RequestParam("idTurma") Turma turma ) {
-		ModelAndView modelAndView = new ModelAndView(FRAGMENT_PROFESSORES);
-		modelAndView.addObject("professores", turma != null ? turma.getProfessores() : null);
-		return modelAndView;
-	}
-
 	@PostMapping("/{idTurma}/status")
 	public ModelAndView alterarStatus(@PathVariable("idTurma") Turma turma, RedirectAttributes attributes) {
 		ModelAndView modelAndView = new ModelAndView(FRAGMENT_STATUS_TURMA);
@@ -182,33 +127,4 @@ public class TurmaController {
 		return modelAndView;
 	}
 	
-	@PreAuthorize(PERMISSAO_ADMINISTRACAO)
-	@GetMapping("/{idTurma}/remover-aluno/{idAluno}")
-	public ModelAndView removerInscricaoDeAluno(@PathVariable("idTurma") Turma turma, 
-			@PathVariable("idAluno") Aluno aluno, RedirectAttributes attributes) {
-		
-		try {
-			turmaService.removerInscricao(turma, aluno);
-			attributes.addFlashAttribute(SUCCESS, SUCCESS_REMOVER_INSCRICAO);
-		} catch (ProntuarioException e) {
-			attributes.addFlashAttribute(ERROR, e.getMessage());
-		}
-		
-		return new ModelAndView(REDIRECT_DETALHES_TURMA + turma.getId());
-	}
-	
-	@PreAuthorize(PERMISSAO_ADMINISTRACAO)
-	@GetMapping("/{idTurma}/remover-professor/{idProfessor}")
-	public ModelAndView removerProfessor(@PathVariable("idTurma") Turma turma, @PathVariable("idProfessor") Servidor professor,
-			RedirectAttributes attributes) {
-		
-		try {
-			turmaService.removerProfessor(turma, professor);
-			attributes.addFlashAttribute(SUCCESS, SUCCESS_DESVINCULAR_PROFESSOR);
-		} catch (ProntuarioException e) {
-			attributes.addFlashAttribute(ERROR, e.getMessage());
-		}
-		
-		return new ModelAndView(REDIRECT_DETALHES_TURMA + turma.getId());
-	}
 }
