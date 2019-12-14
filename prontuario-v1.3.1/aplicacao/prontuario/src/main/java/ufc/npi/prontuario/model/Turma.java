0d58d74c0e9344b4b9b47ed6f640c04a15b12b85
MaikeBezerra <maike_ti@hotmail.com>
2019-12-14 16:57:18 -0300

    removendo feature envy turma service test

--- prontuario-v1.3.1/aplicacao/prontuario/src/main/java/ufc/npi/prontuario/model/Turma.java
+++ prontuario-v1.3.1/aplicacao/prontuario/src/main/java/ufc/npi/prontuario/model/Turma.java
@@ -52,6 +52,14 @@ public class Turma {
 	@OneToMany(mappedBy = "turma", cascade = CascadeType.MERGE)
 	private List<Atendimento> atendimentos;
 
+	public Turma(String nome, int ano, int semestre, Disciplina disciplina, List<Servidor> professores) {
+		this.nome = nome;
+		this.ano = ano;
+		this.semestre = semestre;
+		this.disciplina = disciplina;
+		this.professores = professores;
+	}
+
 	public Integer getId() {
 		return id;
 	}
